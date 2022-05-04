72548e72b8eff16d9eabe9ce9e4f7ece88616819
TTiip <2711430287@qq.com>
2022-05-04 18:14:29 +0800

    feat: 加入打字机功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,44 +6,18 @@
         Vitesse Lite
       </a>
     </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
     <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+      <textarea id="textarea input" placeholder="please input something..." m-4 h-300px w-500px rounded border-2 p-3 />
+      <textarea id="textarea output" placeholder="please input something..." m-4 h-300px w-500px rounded border-2 p-3 />
+      <p id="companionMethods" />
     </div>
+    <div py-4 />
   </div>
 </template>
 
 <script setup lang="ts">
-  const name = $ref('')
-
-  const router = useRouter()
-  const go = () => {
-    if (name) { router.push(`/hi/${encodeURIComponent(name)}`) }
-  }
+  import { start } from '~/utils'
+  onMounted(() => {
+    start()
+  })
 </script>
