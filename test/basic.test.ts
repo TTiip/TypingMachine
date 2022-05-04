3b3652c8abe1274b19133abccceab7338eed684e
TTiip <2711430287@qq.com>
2022-05-04 16:50:33 +0800

    feat: core functions

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,11 +1,98 @@
-import {
-  describe,
-  expect,
-  it
-} from 'vitest'
+import { describe, expect, it } from 'vitest'
+import type { Diff } from 'diff-match-patch'
+import { diff_match_patch as DMP } from 'diff-match-patch'
 
-describe('Hi', () => {
-  it('should works', () => {
-    expect(1 + 1).toEqual(2)
+const input = `
+import { describe, expect, it } from 'vitest'
+import { one } from './src'
+
+describe('should', () => {
+  it('exported', () => {
+    expect(one).toEqual(1)
+  })
+})
+`
+
+const output = `
+import { describe, expect, it } from 'vitest'
+
+describe('should', () => {
+  it('one', () => {
+    expect(one).toEqual(1)
+    expect(two).toEqual(2)
+  })
+})
+`
+interface InsertPatch {
+  type: 'insert'
+  from: number
+  text: string
+}
+
+interface RemovePatch {
+  type: 'remove'
+  from: number
+  length: number
+}
+
+type Patch = InsertPatch | RemovePatch
+
+// 获取比较处理以后的片段
+function diff (inputStr: string, outputStr: string): Diff[] {
+  const differ = new DMP()
+  const diff_main = differ.diff_main(inputStr, outputStr)
+  differ.diff_cleanupEfficiency(diff_main)
+  return diff_main
+}
+
+// 计算节点相关的东西
+function calculatePatch (diffs: Diff[]): Patch[] {
+  const patches: Patch[] = []
+  let index = 0
+  for (const diff of diffs) {
+    if (diff[0] === 0) {
+      index += diff[1].length
+      continue
+    }
+    if (diff[0] === -1) {
+      patches.push({
+        type: 'remove',
+        from: index + diff[1].length,
+        length: diff[1].length
+      })
+    } else if (diff[0] === 1) {
+      patches.push({
+        type: 'insert',
+        from: index,
+        text: diff[1]
+      })
+      index += diff[1].length
+    }
+  }
+  return patches
+}
+
+function applyPatch (inputStr: string, patches: Patch[]): string {
+  let outPutStr = inputStr
+  for (const patch of patches) {
+    if (patch.type === 'insert') {
+      outPutStr = outPutStr.slice(0, patch.from) + patch.text + outPutStr.slice(patch.from)
+    } else if (patch.type === 'remove') {
+      outPutStr = outPutStr.slice(0, patch.from - patch.length) + outPutStr.slice(patch.from)
+    }
+  }
+  return outPutStr
+}
+
+describe('base test', () => {
+  it('exported', () => {
+    const delta = diff(input, output)
+    const patches = calculatePatch(delta)
+    const applyPatches = applyPatch(input, patches)
+
+    expect(delta).toMatchSnapshot('delta')
+    expect(patches).toMatchSnapshot('patches')
+    expect(applyPatches).toMatchSnapshot('applyPatches')
+    expect(applyPatches).toEqual(output)
   })
 })
